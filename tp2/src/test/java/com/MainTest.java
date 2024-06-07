b7673555539ec89e75187216baa242e18c44ce01
Gonzalo <llaos@fi.uba.ar>
2024-06-07 10:41:28 -0300

    Avance

---
+++ tp2/src/test/java/com/MainTest.java
@@ -0,0 +1,13 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test01() {
+        int numero = 1;
+        assertEquals(1, numero);
+    }
+}
